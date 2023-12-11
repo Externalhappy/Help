e7c78281c5618b1a8675f4e301df83061410b954
External_Happy <120341215+Externalhappy@users.noreply.github.com>
2023-12-11 19:04:34 +0400

    Add files via upload
    This is a Python function to select k random samples per class in PyTorch.

---
+++ few_shot.py
@@ -0,0 +1,21 @@
+from torch.utils.data import Subset
+from collections import defaultdict
+
+def build_dataset_dall_e(dataset, num_shot):
+    class_indices = defaultdict(list)
+    for i, element in enumerate(dataset.samples):
+        class_indices[element[1]].append(i)
+    selected_indices = []
+    for indices in class_indices.values():
+        # Check if k is greater than the number of elements in the class
+        if  num_shot> len(indices):
+            print(f"Warning: Class has fewer than {num_shot} elements.")
+            selected_indices.extend(indices)
+        else:
+            selected_indices.extend(random.sample(indices, num_shot))
+    subset_dataset = Subset(dataset, selected_indices)    
+    return subset_dataset
+
+
+
+
