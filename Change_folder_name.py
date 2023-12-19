cbfadcc4548accf42d27e8c3ef6f4f6b7575f610
PhanTrang <120341215+Externalhappy@users.noreply.github.com>
2023-12-19 23:18:20 +0400

    change_folder_name.py

--- Change_folder_name.py
+++ Change_folder_name.py
@@ -3,16 +3,17 @@ import os
 d = {"21": "fire lily", "3": "canterbury bells", "45": "bolero deep blue"}
 
 # Get the list of all items in the directory
-items = os.listdir("./data/flowers/train")
+path = "./data/flowers/train"
+items = os.listdir(path)
 
 # Iterate through each item
 for item in items:
-    item_path = os.path.join("./data/flowers/train", item)
+    item_path = os.path.join(path, item)
     # Check if it's a directory
     if os.path.isdir(item_path):
         # Check if the folder name matches the old_name
         if item in d.keys():
             # Construct the new path with the new folder name
-            new_path = os.path.join("./data/flowers/train", d[item])
+            new_path = os.path.join(path, d[item])
             # Rename the folder
             os.rename(item_path, new_path)
