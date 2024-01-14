a186d4683d9563c0c60fe4ade2090276c60a29e5
PhanTrang <120341215+Externalhappy@users.noreply.github.com>
2024-01-14 23:09:46 +0400

    prototype.py

--- prototype.py
+++ prototype.py
@@ -4,3 +4,7 @@ for i in unique_y:
     idx = labels == i
     means.append(torch.mean(features[idx], dim=0))
 prototypes = torch.stack(means, 0)
+
+
+## or
+# prototypes = torch.stack([torch.mean(features[labels == i], dim=0) for i in unique_y])
